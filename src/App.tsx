44cc56c60bade0c6d29671c6fc1859fcfbbf46a3
Tenor <tenor@fmail.co.uk>
2023-07-26 15:05:46 -0400

    install query param provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
 import "./App.css";
+import { QueryParamProvider } from 'use-query-params';
+import { ReactRouter6Adapter } from 'use-query-params/adapters/react-router-6';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { parse, stringify } from 'querystring';
+
+const Content = (): JSX.Element => {
+  return (
+    <div className="h-full font-[HelveticaNowDisplay] uppercase text-bblue text-3xl text-center p-2 font-medium tracking-wider flex items-center justify-center">DATA EQUITY BANK</div>
+  )
+}
 
 function App() {
   return (
-    <div className="h-full font-[HelveticaNowDisplay] uppercase text-bblue text-3xl text-center p-2 font-medium tracking-wider flex items-center justify-center">DATA EQUITY BANK</div>
+    <BrowserRouter>
+    <QueryParamProvider 
+      adapter={ReactRouter6Adapter}
+      options={{
+        searchStringToObject: parse,
+        objectToSearchString: stringify as any,
+      }}
+    >
+      <Routes>
+        <Route path="/" element={<Content />}/>
+      </Routes>
+    </QueryParamProvider>
+  </BrowserRouter>
   )
 }
 
