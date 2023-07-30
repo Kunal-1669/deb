6a7c4fa30f107c78f6b62d1232d25ef75969ed8e
Tenor <tenor@fmail.co.uk>
2023-07-30 14:42:28 -0400

    implement initial payment history page

--- src/App.tsx
+++ src/App.tsx
@@ -71,6 +71,21 @@ const variants = {
   },
 };
 
+const PaymentHistoryHeader = (): JSX.Element => {
+  return (
+    <div className="w-full rounded-t-2xl py-8 flex flex-col gap-y-2">
+      <div className="text-3xl font-bold font-[Inter] text-bdarkblue leading-none tracking-wide">
+        Transactions
+      </div>
+
+      {/* subheading */}
+      <div className="text-md text-bblue/[0.75] font-medium">
+        Find data wage invoices below.
+      </div>
+    </div>
+  );
+};
+
 const AssetsHeader = (): JSX.Element => {
   return (
     <div className="w-full rounded-t-2xl py-8 flex flex-col gap-y-2">
@@ -86,8 +101,91 @@ const AssetsHeader = (): JSX.Element => {
   );
 };
 
+// TODO: refactor this into useSimulator
+//
+//   example:
+//
+//     const {state, open, close} = useSimulator()
+//
+//     return <button onClick={open}>{state}</button>
+//
 const useSimulatorState = () => {
   return useQueryParam("sim", StringParam);
+};
+
+const PaymentTransactionPlaceholder = (props: {
+  icon?: "dollar" | "bank" | "cash";
+  time?: string
+}): JSX.Element => {
+  const { icon = "dollar", time = "Jul 22" } = props;
+
+  return (
+    <>
+      <div className="flex gap-2 relative items-center">
+        <div className="w-10 h-10 rounded-full bg-boffwhiteandblue text-bblue/[0.5] flex items-center justify-center">
+          {icon === "dollar" ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          ) : icon === "bank" ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0012 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75z"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z"
+              />
+            </svg>
+          )}
+        </div>
+        <div className="grow flex flex-col gap-y-1.5">
+          <div className="h-2 rounded-md w-[60%] bg-bdarkblue/[0.5]"></div>
+          <div className="font-[Inter] font-light leading-none h-2 rounded-md w-[35%] text-bdarkblue/[0.25]">
+            {time}
+          </div>
+        </div>
+        <div className="self-end flex flex-col gap-y-0.5 items-start">
+          <span className="text-green-700 font-bold text-md leading-none flex items-start">
+            <span style={{ fontSize: "0.95em" }}>+</span>
+            <span style={{ fontSize: "0.9em" }}>$</span>
+            <span>25.00</span>
+          </span>
+        </div>
+      </div>
+    </>
+  );
 };
 
 const AssetPlaceholder = (): JSX.Element => {
@@ -203,224 +301,20 @@ const Header = (): JSX.Element => {
 };
 
 const PaymentHistory = (): JSX.Element => {
-  const [simulatorState] = useSimulatorState();
-
-  // TODO: rename this
-  const [walkthroughSkipped, setWalkthroughSkipped] = useQueryParam(
-    "ws",
-    BooleanParam
-  );
-
-  const [walkthroughStep, setWalkthroughStep] = useQueryParam(
-    "step",
-    NumberParam
-  );
-
-  const handleSkip = useCallback(() => {
-    setWalkthroughSkipped(true);
-  }, [setWalkthroughSkipped]);
-
-  const [direction, setDirection] = useState(0);
-
-  const moveCarousel = useCallback(
-    (newDirection: number) => {
-      if (walkthroughStep != null) {
-        const max = steps.length;
-        const newStep = (((walkthroughStep + newDirection) % max) + max) % max;
-        setWalkthroughStep(newStep);
-        setDirection(newDirection);
-        setTimeout(() => {
-          const newStepButtonRef = stepButtonRefs[newStep];
-          if (newStepButtonRef && newStepButtonRef.current) {
-            newStepButtonRef.current.focus();
-          }
-        }, 100);
-      }
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [walkthroughStep, setWalkthroughStep]
-  );
-
-  const handleNext = useCallback(() => {
-    if (walkthroughStep == null) {
-      setWalkthroughStep(1);
-      setTimeout(() => {
-        const newStepButtonRef = stepButtonRefs[1];
-        if (newStepButtonRef && newStepButtonRef.current) {
-          newStepButtonRef.current.focus();
-        }
-      }, 100);
-    } else {
-      moveCarousel(1);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [moveCarousel, setWalkthroughStep, walkthroughStep]);
-
-  const stepButtonRefs = [
-    useRef<HTMLButtonElement>(null),
-    useRef<HTMLButtonElement>(null),
-    useRef<HTMLButtonElement>(null),
-    useRef<HTMLButtonElement>(null),
-  ];
-
-  // TODO: i18n
-  const steps = [
-    <WalkthroughCard key={0}>
-      <Dialog.Title className="font-[Inter] max-w-xs text-blue-50 text-2xl font-black tracking-normal px-5 py-10 -my-2 text-center">
-        Welcome to Data&nbsp;Equity&nbsp;Bank!
-      </Dialog.Title>
-
-      <div className="flex flex-col gap-y-10 mb-10 grow justify-center">
-        <button
-          onClick={handleNext}
-          ref={stepButtonRefs[0]}
-          className="w-48 bg-blue-50 text-blue-800 text-center font-[Inter] font-semibold text-lg rounded-xl leading-none p-3 tracking-tight cursor-pointer relative group"
-        >
-          <div className="absolute inset-0 rounded-xl bg-blue-900/[0.3] opacity-0 group-hover:opacity-25 group-active:opacity-100 h-full w-full z-10"></div>
-          <span>Start Walkthrough</span>
-        </button>
-
-        <button
-          className="w-48 text-blue-50 font-medium text-lg p-3 leading-none rounded-xl text-center font-[Inter] tracking-tight cursor-pointer relative group"
-          onClick={handleSkip}
-        >
-          <div className="absolute inset-0 rounded-xl bg-blue-50/[0.1] opacity-0 group-hover:opacity-100 group-active:opacity-75 h-full w-full z-10"></div>
-          <span>Skip</span>
-        </button>
-      </div>
-    </WalkthroughCard>,
-
-    <WalkthroughCard key={1}>
-      <Dialog.Title className="font-[Inter] max-w-xs text-blue-50 text-2xl font-black tracking-normal px-5 py-10 -my-2">
-        1. Select a simulation.
-      </Dialog.Title>
-
-      <div className="flex flex-col gap-y-10 mb-10 grow justify-end h-full p-5 pb-0">
-        <button
-          ref={stepButtonRefs[1]}
-          className="w-48 text-blue-50 font-medium text-lg p-3 leading-none rounded-xl text-center font-[Inter] tracking-tight cursor-pointer relative group"
-          onClick={handleNext}
-        >
-          <div className="absolute inset-0 rounded-xl bg-blue-50/[0.1] opacity-0 group-hover:opacity-100 group-active:opacity-75 h-full w-full z-10"></div>
-          <span>Next</span>
-        </button>
-      </div>
-    </WalkthroughCard>,
-
-    <WalkthroughCard key={2}>
-      <Dialog.Title className="font-[Inter] max-w-xs text-blue-50 text-2xl font-black tracking-normal px-5 py-10 -my-2">
-        2. Generate data.
-      </Dialog.Title>
-
-      <div className="flex flex-col gap-y-10 mb-10 grow justify-end h-full p-5 pb-0">
-        <button
-          ref={stepButtonRefs[2]}
-          className="w-48 text-blue-50 font-medium text-lg p-3 leading-none rounded-xl text-center font-[Inter] tracking-tight cursor-pointer relative group"
-          onClick={handleNext}
-        >
-          <div className="absolute inset-0 rounded-xl bg-blue-50/[0.1] opacity-0 group-hover:opacity-100 group-active:opacity-75 h-full w-full z-10"></div>
-          <span>Next</span>
-        </button>
-      </div>
-    </WalkthroughCard>,
-
-    <WalkthroughCard key={3}>
-      <Dialog.Title className="font-[Inter] max-w-xs text-blue-50 text-2xl font-black tracking-normal px-5 py-10 -my-2">
-        3. {/* possibly redeem */} Receive data wages.
-      </Dialog.Title>
-
-      <div className="flex flex-col gap-y-10 mb-10 grow justify-end h-full p-5 pb-0">
-        <button
-          ref={stepButtonRefs[3]}
-          onClick={handleSkip}
-          className="w-full bg-blue-50 text-blue-800 text-center font-[Inter] font-semibold text-lg rounded-xl leading-none p-3 tracking-tight cursor-pointer relative group"
-        >
-          <div className="absolute inset-0 rounded-xl bg-blue-900/[0.3] opacity-0 group-hover:opacity-25 group-active:opacity-100 h-full w-full z-10"></div>
-          <span>Start earning data wages</span>
-        </button>
-      </div>
-    </WalkthroughCard>,
-  ];
-
-  const dragControls = useDragControls();
-
-  const [, setSimulatorState] = useSimulatorState();
-
-  const stopSimulation = useCallback(() => {
-    setSimulatorState("closed");
-  }, [setSimulatorState]);
-
   return (
     <>
       <div className="h-full relative p-2 flex flex-col items-stretch justify-start bg-blue-100/[0.8] gap-y-10">
         <Header />
 
-        <Dialog
-          open={!walkthroughSkipped}
-          as="div"
-          className="absolute inset-0 flex justify-center items-center overflow-hidden"
-          initialFocus={stepButtonRefs[walkthroughStep ?? 0]}
-          onClose={handleSkip}
-        >
-          <AnimatePresence initial={false}>
-            <motion.div
-              key={walkthroughStep}
-              custom={direction}
-              variants={variants}
-              initial="enter"
-              animate="center"
-              dragControls={dragControls}
-              exit="exit"
-              transition={{
-                x: { type: "spring", stiffness: 350, damping: 45 },
-                opacity: { duration: 0.2 },
-              }}
-              drag="x"
-              dragConstraints={{ left: 0, right: 0 }}
-              dragElastic={1}
-              onDragEnd={(_e, { offset, velocity }) => {
-                const swipe = swipePower(offset.x, velocity.x);
+        <div className="max-w-md mx-auto w-full border border-bblue/[0.5] pb-5 px-5 rounded-2xl">
+          <PaymentHistoryHeader />
 
-                if (swipe < -swipeConfidenceThreshold) {
-                  moveCarousel(1);
-                } else if (swipe > swipeConfidenceThreshold) {
-                  moveCarousel(-1);
-                }
-              }}
-              className="absolute -left-1/2 -right-1/2 mx-auto w-full max-w-[22.5rem] h-full top-0 bottom-0 max-h-full flex items-center justify-center"
-            >
-              {walkthroughStep == null ? steps[0] : steps[walkthroughStep]}
-            </motion.div>
-          </AnimatePresence>
-        </Dialog>
-
-        <Dialog
-          open={simulatorState === "open"}
-          as="div"
-          className="absolute inset-0 flex justify-center items-center overflow-hidden"
-          initialFocus={stepButtonRefs[walkthroughStep ?? 0]}
-          onClose={handleSkip}
-        >
-          <div className="absolute -left-1/2 -right-1/2 mx-auto w-full max-w-[22.5rem] h-full top-0 bottom-0 max-h-full flex items-center justify-center">
-            <WalkthroughCard height="auto">
-              <Dialog.Title className="font-[Inter] max-w-xs text-blue-50 text-2xl font-black tracking-normal px-5 py-10 -my-2 text-center">
-                Success!
-              </Dialog.Title>
-
-              <div className="font-[Inter] max-w-xs text-blue-50 font-semibold px-5 py-10 text-center">
-                You have completed the simulation.
-              </div>
-
-              <button
-                onClick={stopSimulation}
-                className="block font-[Inter] uppercase text-boffwhite border border-boffwhite rounded-xl px-4 py-3 text-center tracking-wider font-bold relative group cursor-pointer mb-10"
-              >
-                <div className="absolute inset-0 rounded-xl bg-boffwhite/[0.2] opacity-0 group-hover:opacity-50 group-active:opacity-100 h-full w-full z-10"></div>
-                <span>Take Again</span>
-              </button>
-            </WalkthroughCard>
+          <div className="flex flex-col gap-y-10">
+            <PaymentTransactionPlaceholder time="Jun 26" />
+            <PaymentTransactionPlaceholder time="Jun 22" icon="bank" />
+            <PaymentTransactionPlaceholder time="Jul 3" icon='cash' />
           </div>
-        </Dialog>
+        </div>
       </div>
     </>
   );
