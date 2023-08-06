a0ae261ba24f2923c0d69f480f4c780f814121df
Tenor <tenor@fmail.co.uk>
2023-08-06 13:12:21 -0400

    initial card design for simulation selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import cx from 'classnames'
+import cx from "classnames";
 import "./App.css";
 import {
   BooleanParam,
@@ -238,15 +238,15 @@ const AssetPlaceholder = (): JSX.Element => {
 };
 
 const ExplorerModeSelector = (): JSX.Element => {
-  const [explorerMode, setExplorerMode] = useExplorerMode()
+  const [explorerMode, setExplorerMode] = useExplorerMode();
 
   const handleStackClick = useCallback(() => {
-    setExplorerMode('stack')
-  }, [setExplorerMode])
+    setExplorerMode("stack");
+  }, [setExplorerMode]);
 
   const handleCardClick = useCallback(() => {
-    setExplorerMode('card')
-  }, [setExplorerMode])
+    setExplorerMode("card");
+  }, [setExplorerMode]);
 
   const iconClassName = "w-8 h-8";
 
@@ -284,14 +284,68 @@ const ExplorerModeSelector = (): JSX.Element => {
 
   return (
     <div className="mb-10">
-      <button onClick={handleCardClick} className={cx("inline-block border-b-4 p-2", explorerMode !== 'stack' ? 'border-bblack text-bblack' : "text-bblack/[0.5] border-transparent p-2")}>
+      <button
+        onClick={handleCardClick}
+        className={cx(
+          "inline-block border-b-4 p-2",
+          explorerMode !== "stack"
+            ? "border-bblack text-bblack"
+            : "text-bblack/[0.5] border-transparent p-2"
+        )}
+      >
         {rowsIcon}
       </button>
 
-      <button onClick={handleStackClick} className={cx("inline-block border-b-4 p-2", explorerMode === 'stack' ? 'border-bblack text-bblack' : "text-bblack/[0.5] border-transparent p-2")}>
+      <button
+        onClick={handleStackClick}
+        className={cx(
+          "inline-block border-b-4 p-2",
+          explorerMode === "stack"
+            ? "border-bblack text-bblack"
+            : "text-bblack/[0.5] border-transparent p-2"
+        )}
+      >
         {stackIcon}
       </button>
     </div>
+  );
+};
+
+const SimulationListingEntry = (): JSX.Element => {
+  return (
+    <>
+      <div className="flex flex-col gap-y-5">
+        <div
+          style={{
+            backgroundImage: ` url("data:image/svg+xml,%3Csvg width='100' height='100' viewBox='0 0 180 180' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M81.28 88H68.413l19.298 19.298L81.28 88zm2.107 0h13.226L90 107.838 83.387 88zm15.334 0h12.866l-19.298 19.298L98.72 88zm-32.927-2.207L73.586 78h32.827l.5.5 7.294 7.293L115.414 87l-24.707 24.707-.707.707L64.586 87l1.207-1.207zm2.62.207L74 80.414 79.586 86H68.414zm16 0L90 80.414 95.586 86H84.414zm16 0L106 80.414 111.586 86h-11.172zm-8-6h11.173L98 85.586 92.414 80zM82 85.586L87.586 80H76.414L82 85.586zM17.414 0L.707 16.707 0 17.414V0h17.414zM4.28 0L0 12.838V0h4.28zm10.306 0L2.288 12.298 6.388 0h8.198zM180 17.414L162.586 0H180v17.414zM165.414 0l12.298 12.298L173.612 0h-8.198zM180 12.838L175.72 0H180v12.838zM0 163h16.413l.5.5 7.294 7.293L25.414 172l-8 8H0v-17zm0 10h6.613l-2.334 7H0v-7zm14.586 7l7-7H8.72l-2.333 7h8.2zM0 165.414L5.586 171H0v-5.586zM10.414 171L16 165.414 21.586 171H10.414zm-8-6h11.172L8 170.586 2.414 165zM180 163h-16.413l-7.794 7.793-1.207 1.207 8 8H180v-17zm-14.586 17l-7-7h12.865l2.333 7h-8.2zM180 173h-6.613l2.334 7H180v-7zm-21.586-2l5.586-5.586 5.586 5.586h-11.172zM180 165.414L174.414 171H180v-5.586zm-8 5.172l5.586-5.586h-11.172l5.586 5.586zM152.933 25.653l1.414 1.414-33.94 33.942-1.416-1.416 33.943-33.94zm1.414 127.28l-1.414 1.414-33.942-33.94 1.416-1.416 33.94 33.943zm-127.28 1.414l-1.414-1.414 33.94-33.942 1.416 1.416-33.943 33.94zm-1.414-127.28l1.414-1.414 33.942 33.94-1.416 1.416-33.94-33.943zM0 85c2.21 0 4 1.79 4 4s-1.79 4-4 4v-8zm180 0c-2.21 0-4 1.79-4 4s1.79 4 4 4v-8zM94 0c0 2.21-1.79 4-4 4s-4-1.79-4-4h8zm0 180c0-2.21-1.79-4-4-4s-4 1.79-4 4h8z' fill='%231a3f70' fill-opacity='0.4' fill-rule='evenodd'/%3E%3C/svg%3E")`,
+          }}
+          className="w-full aspect-square rounded-xl bg-boffwhiteandblue"
+        ></div>
+        <div className="grow flex flex-col gap-y-1.5 -mb-2">
+          <div className="h-2 rounded-md w-[60%] bg-blue-100"></div>
+          <div className="h-2 rounded-md w-[35%] bg-blue-100/[0.75]"></div>
+          <div className="flex gap-1 items-center text-bblue/[0.75] mt-1">
+
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+  <path strokeLinecap="round" strokeLinejoin="round" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+</svg>
+          <div className="font-[Inter] font-light leading-none">45m</div>
+          </div>
+        </div>
+        <div className="flex gap-1 items-end">
+          <span className="text-bdarkblue font-bold text-xl leading-none flex items-start">
+            <span style={{ fontSize: "0.8em" }} className="translate-y-0.5">
+              $
+            </span>
+            <span>50</span>
+          </span>
+
+          <span className="text-bblue/[0.75] leading-none font-light">
+            /month
+          </span>
+        </div>
+      </div>
+    </>
   );
 };
 
@@ -300,7 +354,7 @@ const AssetSelection = (): JSX.Element => {
 
   return (
     <>
-      <div className="max-w-md mx-auto w-full pb-5 rounded-2xl">
+      <div className="max-w-md mx-auto w-full pb-5 rounded-2xl px-2">
         <ExplorerModeSelector />
 
         {explorerMode === "stack" ? (
@@ -310,7 +364,14 @@ const AssetSelection = (): JSX.Element => {
             <AssetPlaceholder />
           </div>
         ) : (
-          <></>
+          <>
+            <div className="flex flex-col gap-y-12">
+
+            <SimulationListingEntry />
+            <SimulationListingEntry />
+            <SimulationListingEntry />
+            </div>
+          </>
         )}
       </div>
     </>
@@ -396,7 +457,7 @@ const PaymentHistory = (): JSX.Element => {
 
   return (
     <>
-      <div className="h-full relative p-2 flex flex-col items-stretch justify-start bg-blue-100/[0.8] gap-y-10">
+      <div className="h-min min-h-full relative p-2 flex flex-col items-stretch justify-start bg-blue-100/[0.8] gap-y-10">
         <Header />
 
         <div className="max-w-md mx-auto w-full pb-5 px-5 rounded-2xl bg-boffwhite shadow-sm">
@@ -611,9 +672,16 @@ const Home = (): JSX.Element => {
     setSimulatorState("closed");
   }, [setSimulatorState]);
 
+  const [explorerMode] = useExplorerMode();
+
   return (
     <>
-      <div className="h-full relative p-2 flex flex-col items-stretch justify-start bg-blue-100/[0.8] gap-y-10">
+      <div
+        className={cx(
+          "h-min min-h-full relative p-2 flex flex-col items-stretch justify-start gap-y-10",
+          explorerMode === "stack" ? "bg-blue-100/[0.8]" : "bg-blue-100/[0.4]"
+        )}
+      >
         <Header />
 
         <AssetSelection />
